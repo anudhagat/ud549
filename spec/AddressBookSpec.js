0da5030c142aa88ca332a81dd2222dd092256952
anudhagat <anudhagat@yahoo.com>
2015-04-17 15:00:28 -0700

    Update AddressBookSpec.js

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,7 +1,7 @@
 describe('Address Book', function() {
   it('should be able to add a contact', function () {
-    var addressBook = new AddressBook(),
-    thisContact = new Contact();
+    var addressBook = new AddressBook();
+    var thisContact = new Contact();
     
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
